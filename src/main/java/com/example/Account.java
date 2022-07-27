bb15db87b10c9014902cd42854478703e411a49b
Vladimif Fedotov <iagrenadier@gmail.com>
2022-07-27 22:46:09 +0400

    - add code

---
+++ src/main/java/com/example/Account.java
@@ -0,0 +1,12 @@
+package com.example;
+public class Account {
+    private final String name;
+    public Account(String name) {
+        this.name = name;
+    }
+    public boolean checkNameToEmboss() {
+        if (this.name == null) {
+            return false;}
+        return name.matches("(?=.{3,19}$)[a-zA-Zа-яА-Я]+\\s[a-zA-Zа-яА-Я]+");
+    }
+}
